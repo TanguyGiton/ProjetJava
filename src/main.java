98246e734307edd305ea40b47fe97ad78693280b
Iheb Fehri <ihebfehri17@gmail.com>
2016-01-08 22:33:38 +0100

    Ajouté la base de la chose. (Une boulle qui bouge sur l'écran) ajouté la hierarchie du projet

--- src/main.java
+++ src/main.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
 import gui.board;
 import javax.swing.JFrame;
 import java.awt.Graphics;
